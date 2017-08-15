0fd65255579af524c1b6943d0bfbfa77993d2f35
logonrm <logonrm@fiap.com.br>
2017-08-15 20:55:37 -0300

    Alteracao na rota de pesquisa

--- src/main/java/br/com/tsi/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/tsi/notepadsaas/controller/NotaController.java
@@ -2,15 +2,20 @@ package br.com.tsi.notepadsaas.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tsi.model.Nota;
 import br.com.tsi.notepadsaas.repository.NotaRepository;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value="/nota")
 public class NotaController {
 
 	private NotaRepository noteRepository;
@@ -27,5 +32,11 @@ public class NotaController {
 	@PostMapping( value = "/" )
 	public void save(@RequestBody Nota nota) {
 		noteRepository.save(nota);
+	}
+	
+	@GetMapping(value = "/titulo/{titulo}")
+	private List<Nota> findByTitulo(@PathVariable(value="titulo") String q)
+	{
+		return noteRepository.findByTitulo(q);
 	}
 }
--- src/main/java/br/com/tsi/notepadsaas/repository/NotaRepository.java
+++ src/main/java/br/com/tsi/notepadsaas/repository/NotaRepository.java
@@ -1,8 +1,11 @@
 package br.com.tsi.notepadsaas.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import br.com.tsi.model.Nota;
 
 public interface NotaRepository extends MongoRepository<Nota,String> {
 
+	List<Nota> findByTitulo(String titulo);
 }
